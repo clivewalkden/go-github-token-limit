94f2d0bdb3532e506f451e064bd68e9f26f489fc
Clive Walkden <clive@sozodesign.co.uk>
2024-06-15 22:53:53 +0100

    test: Helper test added

---
+++ internal/utils/helper_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCenterString_WithNormalString(t *testing.T) {
+	result := CenterString("test", 10)
+	assert.Equal(t, "   test   ", result)
+}
+
+func TestCenterString_WithEmptyString(t *testing.T) {
+	result := CenterString("", 10)
+	assert.Equal(t, "          ", result)
+}
+
+func TestCenterString_WithCenteredString(t *testing.T) {
+	result := CenterString("   test   ", 10)
+	assert.Equal(t, "   test   ", result)
+}
+
+func TestCenterString_WithLongString(t *testing.T) {
+	result := CenterString("this is a long string", 10)
+	assert.Equal(t, "this is a long string", result)
+}
+
+func TestCenterString_WithOddWidth(t *testing.T) {
+	result := CenterString("test", 11)
+	assert.Equal(t, "   test    ", result)
+}
